58880be4fc6c80b5ff3941fccc4e697c0b355483
adiogoti <andrei.diogoti@endava.com>
2020-05-03 18:30:01 +0300

    Staircase file is added

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class First {
-}
+        static String[] word = {"ABADBA", "ABBA", "Ford", "reviver"};
+
+        static boolean   palindromeValidation(String j)
+        {
+            String  reverse = "";
+            int length = j.length();
+            for ( int i = length - 1; i >= 0; i-- )
+                reverse = reverse + j.charAt(i);
+            if (j.equals(reverse))
+                return true;
+            else
+                return false;
+        }
+
+        public static void main(String args[]){
+
+            for (String j : word) {
+                new First().palindromeValidation(j);
+
+                    if (palindromeValidation(j) == true)
+                        System.out.println("Entered string/number " + j + " is a palindrome.");
+                    else
+                        System.out.println("Entered string/number " + j + " isn't a palindrome.");
+
+            }
+        }
+    }
+
+
--- src/main/java/Staircase.java
+++ src/main/java/Staircase.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
 
 public class Staircase {
+
+    public static void main(String[]args)
+    {
+        Staircase scan = new Staircase();
+
+        int n = 6;
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if(i >= j)
+                    {
+                        System.out.print("#");
+                    }
+                }
+                System.out.println();
+            }
+    }
 }
